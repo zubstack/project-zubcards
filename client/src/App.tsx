cc7a1c20dc98ed5340663b2b7e6e3280659357a4
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-01-12 08:18:04 -0500

    client with typescript and scss

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Hello
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
