ccab771708af9bda54ece92ecb9ecb07e0e712a5
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-01-19 20:08:32 -0500

    remake layout /decks

--- client/src/pages/Decks/Decks.tsx
+++ client/src/pages/Decks/Decks.tsx
@@ -7,7 +7,6 @@ import OptionsButton from "../../ui/OptionsButton/OptionsButton";
 import CreateDeckModal from "../../components/CreateDeckModal/CreateDeckModal";
 import endpoints from "../../services/api/endpoints";
 import { Link } from "react-router-dom";
-import PageLayout from "../../layout/PageLayout/PageLayout";
 
 function Decks() {
   const [decks, setDecks] = useState(null);
@@ -61,60 +60,65 @@ function Decks() {
   }, []);
 
   return (
-    <PageLayout>
-      <div className="dashboard__top">
-        <h2>Dashboard</h2>
-        <TextButton onClick={handleOpenDecksCreateModal}>
-          Create deck
-        </TextButton>
+    <main id="decks__main">
+      <div className="container">
+        <div className="container__top">
+          <h2>Dashboard</h2>
+          <TextButton onClick={handleOpenDecksCreateModal}>
+            Create deck
+          </TextButton>
+        </div>
+        <div className="table__container">
+          {decks ? (
+            <table id="decks__table">
+              <thead>
+                <tr>
+                  <th>Deck</th>
+                  <th>New</th>
+                  <th>Learn</th>
+                  <th>Due</th>
+                  <th></th>
+                </tr>
+              </thead>
+              {decks.map((deck) => (
+                <tbody key={deck.topic}>
+                  <tr>
+                    <td>
+                      <Link to={`/cards?deckId=${deck.id}`}>{deck.topic}</Link>
+                    </td>
+                    <td>{deck.new}</td>
+                    <td>{deck.learn}</td>
+                    <td>{deck.due}</td>
+                    <td>
+                      <div onClick={handleToggle} className="options">
+                        <RiSettings5Fill />
+                        <div className="options__menu">
+                          <OptionsButton onClick={() => handleEdit(deck.id)}>
+                            Edit
+                          </OptionsButton>
+                          <OptionsButton onClick={() => handleDelete(deck.id)}>
+                            Delete
+                          </OptionsButton>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+          ) : (
+            <p>No content to show</p>
+          )}
+        </div>
+        <CreateDeckModal
+          isOpen={isDecksCreateModalOpen}
+          onClose={closeOpenDecksCreateModal}
+          fetchData={fetchData}
+          editId={editId}
+          setEditId={setEditId}
+        />
       </div>
-      {decks ? (
-        <table className="dashboard__container">
-          <thead className="dashboard__headers">
-            <tr>
-              <th className="dashboard__topic">Deck</th>
-              <th>New</th>
-              <th>Learn</th>
-              <th className="dashboard__last">Due</th>
-            </tr>
-          </thead>
-          {decks.map((deck) => (
-            <tbody className="dashboard__items" key={deck.topic}>
-              <tr>
-                <td className="dashboard__topic">
-                  <Link to={`/cards?deckId=${deck.id}`}>{deck.topic}</Link>
-                </td>
-                <td className="dashboard__items--blue">{deck.new}</td>
-                <td className="dashboard__items--red">{deck.learn}</td>
-                <td className="dashboard__items--green">{deck.due}</td>
-                <td className="dashboard__last ">
-                  <div onClick={handleToggle} className="options">
-                    <RiSettings5Fill />
-                    <div className="options__menu">
-                      <OptionsButton onClick={() => handleEdit(deck.id)}>
-                        Edit
-                      </OptionsButton>
-                      <OptionsButton onClick={() => handleDelete(deck.id)}>
-                        Delete
-                      </OptionsButton>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          ))}
-        </table>
-      ) : (
-        <p>No content to show</p>
-      )}
-      <CreateDeckModal
-        isOpen={isDecksCreateModalOpen}
-        onClose={closeOpenDecksCreateModal}
-        fetchData={fetchData}
-        editId={editId}
-        setEditId={setEditId}
-      />
-    </PageLayout>
+    </main>
   );
 }
 
